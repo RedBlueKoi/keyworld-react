fcd2dfcedf0b90d720e3daa7bf63ccd781046696
RedBlueKoi <bragin.inred@gmail.com>
2022-07-11 15:42:40 +0200

    mdi icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,19 @@ import "./App.css"
 import { Routes, Route, Link } from "react-router-dom"
 import About from "./views/About"
 import Home from "./views/Home"
+import Icon from "@mdi/react"
+import { mdiHome } from "@mdi/js"
 
 function App() {
   return (
     <div className="App">
-      <Link to="about">About</Link>
-      <Link to="/">Home</Link>
+      <div className="flex justify-center items-center gap-4">
+        <Link to="/" className="flex items-center">
+          Home
+          <Icon path={mdiHome} title="Home link" size={1} />
+        </Link>
+        <Link to="about">About</Link>
+      </div>
       <header className="App-header">
         <Routes>
           <Route path="/" element={<Home />} />
