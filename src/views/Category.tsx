bb98cf878544f0c481f06de897bbd0e4a4be9522
spartanny <tq_29@hotmail.com>
2022-07-16 19:08:27 +0530

    Fix the formatting of files

--- src/views/Category.tsx
+++ src/views/Category.tsx
@@ -1,12 +1,12 @@
 import { FC } from "react"
 
-const Category : FC = () => {
-    return(
-        <div>
-            {/* This will use the category component */}
-            Hi this is the Category page !
-        </div>
-    )
+const Category: FC = () => {
+  return (
+    <div>
+      {/* This will use the category component */}
+      Hi this is the Category page !
+    </div>
+  )
 }
 
 export default Category
