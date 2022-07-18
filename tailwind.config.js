f2dd1189dac21809fad3366b736f98dfc3577533
Ozzy <ozzy@ozi-mac.local>
2022-07-18 22:45:13 +0300

    Removed unnecessary shadow definition

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,6 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      boxShadow: {
-        "3xl": "2px 3px 5px rgba(0, 0, 0, 0.25)"
-      }
     }
   },
   plugins: []
