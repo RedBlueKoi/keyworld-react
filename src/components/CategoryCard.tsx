8f236a1e0a8c7c7770367763bff181a1e16e1c01
Ozzy <ozzy@ozi-mac.local>
2022-07-18 22:52:10 +0300

    Refactoring of CategoryCard component

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,9 +1,19 @@
 import { FC } from "react"
+import { Link } from "react-router-dom"
 
-const CategoryCard: FC = () => {
-  return <div className="bg-slate-700 rounded-lg p-3 shadow-3xl w-[220px] h-[220px]">
-        Category Name
-    </div>
+interface Props {
+  name: string
+  linkTo: string
+  img?: string
+}
+const CategoryCard: FC<Props> = (props: Props) => {
+  return (
+    <Link
+      to={props.linkTo}
+      className="bg-slate-700 rounded-lg p-3 shadow-2xl w-[220px] h-[220px]">
+      {props.name}
+    </Link>
+  )
 }
 
 export default CategoryCard
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,17 +1,19 @@
 import { FC } from "react"
 import CategoryCard from "@/components/CategoryCard"
+
 const Home: FC = () => {
+  const categories = [
+    { name: "Category 1", linkTo: "/categories/1", img: "" },
+    { name: "Category 2", linkTo: "/categories/2", img: "" },
+    { name: "Category 3", linkTo: "/categories/3", img: "" }
+  ]
   return (
     <div>
       <h1 className={"text-black text-left mb-6 px-3"}>Categories</h1>
       <div className="flex space-between gap-3 flex-wrap px-3">
-        <CategoryCard />
-        <br />
-        <CategoryCard />
-        <br />
-        <CategoryCard />
-        <br />
-        <CategoryCard />
+        {categories.map((category, key) => (
+          <CategoryCard {...category} key={key} />
+        ))}
       </div>
     </div>
   )
