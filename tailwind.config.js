c80d533e9e04d3bc7efe52005567c21b35df32d9
Ozzy <ozzy@ozi-mac.local>
2022-07-18 23:34:25 +0300

    added primary and accent colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      colors: {
+        primary: "#34495E",
+        accent: "#41B883"
+      }
     }
   },
   plugins: []
