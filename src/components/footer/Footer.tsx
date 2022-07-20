c2e4199e1ba30cf78b8c466d9b0b4975f4b13c68
RedBlueKoi <bragin.inred@gmail.com>
2022-07-20 21:43:37 +0200

    feat: contributors update

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,79 +1,14 @@
 import { FC } from "react"
+import Contributors from "./Contributors"
 
 const Footer: FC = () => {
   return (
     <footer className="bg-primary text-white" id="bottom">
-      <div className="flex items-center justify-center py-7 lg:text-4xl text-3xl font-semibold">
-        <div>
-          <span className="text-teal-400">Welcome</span> to our website
-        </div>
+      <div className="flex items-center justify-center py-4 lg:text-2xl text-xl font-semibold">
+        <div className="text-accent">Contributors:</div>
       </div>
-      <ul className="flex justify-center items-center flex-wrap gap-4 p-4">
-        <li>
-          <a
-            target="_blank"
-            className="link-primary"
-            rel="noreferrer"
-            href="https://github.com/amarborz">
-            Umar Kantaev
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            className="link-primary"
-            rel="noreferrer"
-            href="https://github.com/cartpusher">
-            David Omar
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            className="link-primary"
-            rel="noreferrer"
-            href="https://github.com/Kayvin59">
-            Kayvin
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            className="link-primary"
-            rel="noreferrer"
-            href="https://github.com/maxi-nebula">
-            Shyni Madhumathi Ravichandran
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            className="link-primary"
-            rel="noreferrer"
-            href="https://github.com/rodaw92">
-            RAED (ROY) AWILL
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            className="link-primary"
-            rel="noreferrer"
-            href="https://github.com/spartanny">
-            Tanishq
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            className="link-primary"
-            rel="noreferrer"
-            href="https://github.com/yuzdzhan">
-            Yuzdzhan
-          </a>
-        </li>
-      </ul>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 text-center pt-2 pb-8 text-gray-400 text-sm">
+      <Contributors />
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 text-center pt-2 pb-4 text-gray-400 text-sm">
         <span>©2022 Project. All rights reserved.</span>
         <span>
           <a href="#" className="link-primary">
