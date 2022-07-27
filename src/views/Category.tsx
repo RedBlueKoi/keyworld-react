733bf69d48593a96bf7240db2a33bbb72ff13e79
Ivan <ivanwhitemac@gmail.com>
2022-07-27 22:56:15 +0100

    Changed categories from flex to grid. Added correct shadow. Repleaced h1 with PageTitle component.

--- src/views/Category.tsx
+++ src/views/Category.tsx
@@ -1,28 +1,27 @@
 import { CategoryCard } from "@/components/CategoryCard"
+import PageTitle from "@/components/PageTitle/PageTitle"
 import { FC } from "react"
 
 const Category: FC = () => {
   return (
     // TODO change margin in global CSS for all pages
-    <div className="mx-8">
+    <div>
       {/* This will use the category component */}
       <div className="flex justify-between mt-[50px]">
-        <h1 className="text-4xl">First Category</h1>
+        <PageTitle title="First Category" />
         <div className="flex justify-between mt-[50px]">
           {/* TODO add search icon */}
-          <input type="search" className="drop-shadow-2xl" />
+          <input type="search" className="shadow-2xl" />
           <p className="ml-[30px]">&lt;1 / 4&gt;</p>
         </div>
       </div>
-      <div className="flex justify-between mt-[50px]">
-        <div className="bg-[#34495E] w-[428px] h-[335px] drop-shadow-2xl"></div>
-        <div className="bg-[#34495E] w-[428px] h-[335px] drop-shadow-2xl"></div>
-        <div className="bg-[#34495E] w-[428px] h-[335px] drop-shadow-2xl"></div>
-      </div>
-      <div className="flex justify-between mt-[50px]">
-        <div className="bg-[#34495E] w-[428px] h-[335px] drop-shadow-2xl"></div>
-        <div className="bg-[#34495E] w-[428px] h-[335px] drop-shadow-2xl"></div>
-        <div className="bg-[#34495E] w-[428px] h-[335px] drop-shadow-2xl"></div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mt-8">
+        <div className="bg-primary w-[428px] h-[335px] shadow-2xl"></div>
+        <div className="bg-primary w-[428px] h-[335px] shadow-2xl"></div>
+        <div className="bg-primary w-[428px] h-[335px] shadow-2xl"></div>
+        <div className="bg-primary w-[428px] h-[335px] shadow-2xl"></div>
+        <div className="bg-primary w-[428px] h-[335px] shadow-2xl"></div>
+        <div className="bg-primary w-[428px] h-[335px] shadow-2xl"></div>
       </div>
       <div className="flex justify-end mt-[50px]">
         <p>&lt;1 / 4&gt;</p>
