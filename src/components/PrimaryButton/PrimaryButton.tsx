98d8e31d6fdb70fb6219114c76d7730d6382b854
RedBlueKoi <bragin.inred@gmail.com>
2022-07-31 16:55:27 +0200

    feat: finished button

---
+++ src/components/PrimaryButton/PrimaryButton.tsx
@@ -0,0 +1,41 @@
+import { FC, useMemo } from "react"
+
+interface Props {
+  children: JSX.Element | string
+  type?: "button" | "submit" | "reset"
+  color?: "accent" | "primary"
+  isFloating?: boolean
+  onClick: () => void
+  disabled?: boolean
+  className?: string
+}
+
+const PrimaryButton: FC<Props> = (props: Props) => {
+  const { children, type, disabled, onClick, isFloating, color, className } =
+    props
+  const styles: string = useMemo(() => {
+    const styleArray = [
+      "px-6",
+      "h-10",
+      "inline-flex",
+      "justify-between",
+      "items-center",
+      "text-white",
+      "rounded-md"
+    ]
+    if (isFloating) styleArray.push("shadow-md shadow-primary")
+    styleArray.push(color ? "bg-accent" : "bg-primary")
+    return [...styleArray, className].join(" ")
+  }, [isFloating, color, className])
+  return (
+    <button
+      type={type || "button"}
+      disabled={disabled}
+      onClick={onClick}
+      className={styles}>
+      {children}
+    </button>
+  )
+}
+
+export default PrimaryButton
