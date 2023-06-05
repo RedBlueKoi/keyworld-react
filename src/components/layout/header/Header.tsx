2bf9d3d909a05f190f780b94bee0bf5d3b4d862f
RedBlueKoi <bragin.inred@gmail.com>
2023-06-05 11:44:03 +0200

    header layout

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,6 +1,23 @@
 import "./header.css"
+import Icon from "@mdi/react"
+import { mdiMenu, mdiCart } from "@mdi/js"
 const Header = () => {
-  return <header className="bg-emerald-500 text-white h-10">Header</header>
+  return (
+    <header className="bg-emerald-500 text-sky-900 h-14 px-2 md:px-4 sticky top-0 flex gap-2 md:gap-4 justify-between items-center shadow-sm shadow-gray-500">
+      <a className="flex-shrink-0 text-2xl leading-3" href="/">
+        KeyWorld
+      </a>
+      <input type="text" />
+      <div className="flex gap-2 md:gap-4">
+        <button type="button">
+          <Icon path={mdiCart} size={1} />
+        </button>
+        <button type="button">
+          <Icon path={mdiMenu} size={1.5} />
+        </button>
+      </div>
+    </header>
+  )
 }
 
 export default Header
